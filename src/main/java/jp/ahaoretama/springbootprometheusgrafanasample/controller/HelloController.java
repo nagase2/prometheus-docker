12710a21172d35378e72f8a301b86a8e7e8d39dc
Naggi <nagase2@gmail.com>
2018-12-16 01:13:30 +0900

    up

--- src/main/java/jp/ahaoretama/springbootprometheusgrafanasample/controller/HelloController.java
+++ src/main/java/jp/ahaoretama/springbootprometheusgrafanasample/controller/HelloController.java
@@ -1,6 +1,9 @@
 package jp.ahaoretama.springbootprometheusgrafanasample.controller;
 
 import io.prometheus.client.spring.web.PrometheusTimeMethod;
+
+import java.util.Random;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,6 +16,27 @@ public class HelloController {
     @GetMapping("/hello")
     @PrometheusTimeMethod(name = "hello_controller_say_hello_duration_seconds", help = "Some helpful info here")
     public String sayHello() {
+        Random rand = new Random(); 
+        int waitingTime = rand.nextInt(50); 
+        try {
+            Thread.sleep(waitingTime);
+        } catch (InterruptedException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
         return "hello";
+    }
+    @GetMapping("/hello_wait")
+    @PrometheusTimeMethod(name = "hello_wait_controller_say_hello_duration_seconds", help = "Some helpful info here")
+    public String sayHelloWait() {
+        Random rand = new Random(); 
+        int waitingTime = rand.nextInt(5000); 
+        try {
+            Thread.sleep(waitingTime);
+        } catch (InterruptedException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        return "hello with waiting time of "+ waitingTime;
     }
 }
